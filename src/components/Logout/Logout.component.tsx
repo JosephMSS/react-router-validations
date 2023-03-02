566c556cedeee8010e3d3f1b4e955ab9cca69d39
Joseph Morales Silva <josephzx1@hotmail.com>
2023-03-01 21:52:48 -0600

    Logout: remove data from localStorage and reducer

--- src/components/Logout/Logout.component.tsx
+++ src/components/Logout/Logout.component.tsx
@@ -1,12 +1,14 @@
+import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { PUBLIC_ROUTES } from '../../models'
+import { resetUser } from '../../redux/states/user'
 import { clearLocalStorage, LOCAL_STORAGE_ITEMS } from '../../utilities'
 
 export default function Logout() {
     const navigate = useNavigate()
-
+    const dispatch=useDispatch()
     const logout = () => {
-        clearLocalStorage(LOCAL_STORAGE_ITEMS.USER)
+        dispatch(resetUser())
         navigate(`/${PUBLIC_ROUTES.LOGIN}`, { replace: true });
     }
     return (
